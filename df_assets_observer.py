fe1d8e97b39c1280f62bd2c987c0bb14f2afc783
Patrick Coelho <100944831+riquelmept@users.noreply.github.com>
2026-01-24 14:02:22 -0300

    Create df_assets_observer.py

---
+++ df_assets_observer.py
@@ -0,0 +1,35 @@
+import requests
+import pandas as pd
+
+base_url = "http://services.repcenter.skf.com"
+port = ""
+username = ""
+password = ""
+timeout = 60  # delay de espera em segundos
+
+token_url = f"{base_url}:{port}/token"
+payload = {
+    "grant_type": "password",
+    "username": username,
+    "password": password
+}
+headers = {"Content-Type": "application/x-www-form-urlencoded"}
+
+try:
+    token_response = requests.post(token_url, data=payload, headers=headers, timeout=timeout)
+    token = token_response.json().get("access_token") if token_response.status_code == 200 else None
+except:
+    token = None
+
+#Função para obter os dados
+def get_data(endpoint):
+    try:
+        url = f"{base_url}:{port}{endpoint}"
+        headers = {"Authorization": f"Bearer {token}"}
+        r = requests.get(url, headers=headers, timeout=timeout)
+        return r.json() if r.status_code == 200 else None
+    except:
+        return None
+
+assets = get_data("/v2/assets")
+df_assets = pd.DataFrame(assets) if assets else pd.DataFrame([{"erro": "Falha ao obter /v2/assets"}])
